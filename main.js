bb18a53eb32a79296e0173b117e27fd0f7299031
mumu17-git <moonpapa1709@gmail.com>
2024-12-11 11:42:00 +0900

    aaa

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function getSvgCoord(svgInfo) {
         }
         const pathElement = pathElements[c];
         pathStr += "(";
-        console.log(pathElement.getTotalLength());
+        //console.log(pathElement.getTotalLength());
 
         for(var i = 0;i < pathElement.getTotalLength();i++) {
             const pt = pathElement.getPointAtLength(i);
@@ -45,14 +45,55 @@ function getSvgCoord(svgInfo) {
             pathStr += ",";
             pathStr += String(pt.y);
             pathStr += ",";
-            console.log(i,pt);
+            //console.log(i,pt);
         }
         pathStr += ")";
         pathStr = pathStr.replace(",)",")");
     }
 
-    console.log(pathStr);
+    //console.log(pathStr);
+
+    cdata.push(pathStr);
 }
 
+function objLoad(str) {
+    document.body.insertAdjacentHTML("beforeend",'<object type="image/svg+xml" data="mplus_stroke/data/'+str+'.svg" onload="getSvgInfoFromObjectElement(this).then((svgInfo) => {getSvgCoord(svgInfo);});" width="300" height="300"></object>');
+}
+
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest();
+    req.open("get", "data.csv", true);
+    req.send(null);
+		
+    req.onload = function(){
+	    //console.log(convertCSVtoArray(req.responseText));
+        var arr = convertCSVtoArray(req.responseText);
+        for(var i = 0;i < arr.length;i++) objLoad(arr[i]);
+    }
+}
+
+
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){
+    return str.split("\n");
+}
+
+var cdata = [];
+
+function downloadCSV() {
+    const filename = "data.csv";
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+    
+    const blob = new Blob([bom, cdata], { type: "text/csv" });
+   
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = URL.createObjectURL(blob);
+    link.click();
+
+    URL.revokeObjectURL(link.href);
+}
   
-document.body.insertAdjacentHTML("beforeend",'<object type="image/svg+xml" data="mplus_stroke/kanji-01/亜.svg" onload="getSvgInfoFromObjectElement(this).then((svgInfo) => {console.log(svgInfo.element);getSvgCoord(svgInfo);});" width="300" height="300"></object>');
+
+getCSV();
